867d74cf6be334471c652423a32451a46d5fc2da
Nihil02 <samuelalva02@gmail.com>
2023-04-02 23:22:54 -0600

    prefix

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -1,10 +1,10 @@
-import {} from "../src/infixPrefix";
+import infixPrefix from "../src/infixPrefix";
 import infixPostfix from "../src/infixPostfix";
 import { describe, expect, it } from "vitest";
 
 describe("postfix", () => {
   it("test 1", () => {
-    expect(infixPostfix("a+b")).toBe("ab+");
+    expect(infixPostfix("(a+b)")).toBe("ab+");
   });
   it("test 2", () => {
     expect(infixPostfix("a+b-c")).toBe("ab+c-");
@@ -12,7 +12,22 @@ describe("postfix", () => {
   it("test 3", () => {
     expect(infixPostfix("((a-b)-c*(d/e))")).toBe("ab-cde/*-");
   });
-  it("test 3", () => {
+  it("test 4", () => {
     expect(infixPostfix("x=a")).toBe("xa=");
+  });
+});
+
+describe("prefix", () => {
+  it("test 1", () => {
+    expect(infixPrefix("(a+b)")).toBe("+ab");
+  });
+  it("test 2", () => {
+    expect(infixPrefix("a+b-c")).toBe("-+abc");
+  });
+  it("test 3", () => {
+    expect(infixPrefix("((a-b)-c*(d/e))")).toBe("--ab*c/de");
+  });
+  it("test 4", () => {
+    expect(infixPrefix("x=a")).toBe("=xa");
   });
 });
